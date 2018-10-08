a913c93d4eb97eac6785e00f5fe04480dd9fd643
Arslan <saeed.arslan07@gmail.com>
2018-10-08 16:20:58 -0400

    Collection view implemented

---
+++ Flix/Views/PosterCell.swift
@@ -0,0 +1,15 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Arslan Saeed on 10/8/18.
+//  Copyright © 2018 Arslan Saeed. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
